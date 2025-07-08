ad810c97cb51f3a12cab9851807166c8266c1e74
camilo98-jpg <clagosretamal@gmail.com>
2025-07-07 20:13:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
+<?php
 
+echo "Hola Mundo";
+
+?>
+
+<br>
+
+<?php
+
+for ($i = 0; $i <= 5; $i++) {
+
+
+
+ echo $i."<br>";
+
+
+
+}
+
+// Ejemplo de condicional con operadores
+
+$edad = 30;
+
+if ($edad >= 18 && $edad < 65) {
+
+ echo "Adulto en edad laboral";
+
+} else if ($edad >= 65) {
+
+ echo "Adulto en edad de jubilación";
+
+} else {
+
+ echo "Menor de edad";
+
+}
+
+?>
